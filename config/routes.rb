1e5f3ddd4c1123e0f06aa55921112f607e2210c1
Gennady Kudinov <gennady71@gmail.com>
2022-08-01 20:49:19 +0300

    добавлена миграция Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index
 
-  get '/contacts' => 'static_pages#show'
   get '/' => 'home#index'
+  get '/contacts' => 'static_pages#show'
+  get 'home/index'
 
+  resource :contacts, only: [:new, :create] # Resource определенный метод который принимает символ :Contacts
+  # а дальше идет хешь,  ONLY это ключ хеша, а значение этого Хеша Массив С символами :new, :create методов
+  # которые хотим использовать :new, :create
   resources :clients
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_143337) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_173908) do
   create_table "clients", force: :cascade do |t|
     t.string "number_auto"
     t.string "brand_auto"
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_143337) do
     t.string "phone"
     t.integer "price"
     t.date "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.text "massage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
