fb35487cea323267005baf02d6e5e3f40ece65f5
Gennady Kudinov <gennady71@gmail.com>
2022-08-01 19:05:01 +0300

    add static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get '/' => 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/contacts' => 'static_pages#show'
+  get '/' => 'home#index'
 
   resources :clients
 
