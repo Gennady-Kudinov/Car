70f68bea5997cdadff64c3f263813eecd4dfe011
Gennady Kudinov <gennady71@gmail.com>
2022-08-02 20:36:10 +0300

    ошибка о выводе сообщения

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index
 
-  get '/' => 'home#index'
-  get '/contacts' => 'static_pages#show'
+  get '/' => 'home#index' # Метод вызывает открытия главную страницу без папок
+  get '/contact' => 'static_pages#contact'  # Метод открывает контакты и адрес автосервиса из папки static_pages
   get 'home/index'
+  get 'clients' => 'clients#new' # Методо возвращает форму заполнения после ее ввода
 
   # resource это такой метод который делает маршрут по Path (патерну) Рест (REST) и в Патерне Рест так определено
   # что НЬЮ получает GET  Крейт получает POST
-  resource :contacts, only: [:new, :create] # Resource определенный метод который принимает символ :Contacts
+  get 'contacts' => 'contacts#new' # Можно заменить на resource :contacts, only: [:new, :create], path_names: { :new => '' }
+  resource :contacts, only: [:new, :create]
+  resources :articles
   # а дальше идет хешь,  ONLY это ключ хеша, а значение этого Хеша Массив С символами :new, :create методов
   # которые хотим использовать :new, :create
   resources :clients, only: [:new, :create]
