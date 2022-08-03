b53eea5a73288b08d0b50564c372a3dd2c5a975f
Gennady Kudinov <gennady71@gmail.com>
2022-08-03 20:30:56 +0300

    node

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import * as bootstrap from "bootstrap"
 import "@hotwired/turbo-rails"
 import "controllers"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+
+Rails.start()
+Turbolinks.start()
+
