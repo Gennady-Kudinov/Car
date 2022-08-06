93e1dab732c2d25db675c2afd8642bbf8c8c7c43
Gennady Kudinov <gennady71@gmail.com>
2022-08-06 21:27:32 +0300

    добавлен метод создания папки и дополнительных записей клиентов

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,11 +1,23 @@
 class ClientsController < ApplicationController
 
   def new
+
   end
 
   def create
     @client = Client.new(client_params)
-    @client.save
+    if @client.save
+      response = FileUtils.mkdir_p "BAZA/#{client_params[:brand_auto]}/#{client_params[:model_auto]}/#{client_params[:number_auto]}"
+
+      database_file = File.new('BAZA/database.txt', 'a+')
+      database_file.puts "#{client_params[:number_auto]}  #{client_params[:brand_auto]}  #{client_params[:model_auto]}  #{client_params[:km]}км.  Сумма #{client_params[:price]} Телефон #{client_params[:phone]} Дата #{client_params[:data]}"
+      database_file.close
+
+      id_client = File.new("BAZA/#{client_params[:brand_auto]}/#{client_params[:model_auto]}/#{client_params[:number_auto]}/#{client_params[:number_auto]}.html", 'a+')
+      id_client.puts "<body>#{client_params[:number_auto]} #{client_params[:brand_auto]} #{client_params[:model_auto]} #{client_params[:km]}км. Тип ЭБУ #{client_params[:swid]}: Сумма #{client_params[:price]} Телефон #{client_params[:phone]} Дата #{client_params[:data]}<br />#{client_params[:deffect]}<br /><body>"
+      id_client.close
+
+    end
   end
 
   private
